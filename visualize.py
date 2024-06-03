6cd257e471c25fcf42ffe890ba137b7827e75b45
LM1997610 <115407939+LM1997610@users.noreply.github.com>
2024-06-03 19:25:00 +0200

    Add files via upload

---
+++ visualize.py
@@ -0,0 +1,24 @@
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+fixed_position = {1:(2.0, 5.5), 2:(3.1, 5.1), 3:(1.2, 4.1), 4:(2.3, 3.7), 5:(3.5, 3.7),
+                   6:(3.5, 2.0), 7:(1.5, 2.0), 8:(2.5, 1.3), 9:(4.0, 0.5), 10:(3.0, 0)}
+
+def show_mincut(my_graph, edges_to_color):
+
+    pos = nx.spring_layout(my_graph, pos=fixed_position, fixed = fixed_position.keys())
+
+    fig, ax = plt.subplots(figsize=(5,4))
+    nx.draw_networkx_nodes(my_graph, node_size=750, alpha = 0.15, node_color='green', pos = pos)
+
+
+    nx.draw_networkx_edges(my_graph, edgelist= edges_to_color, edge_color = "red", pos=pos, style=':', )
+
+    nx.draw_networkx_edges(my_graph, alpha=0.15, edge_color = "black", pos=pos, style='-', )
+
+    text = nx.draw_networkx_labels(my_graph, pos, font_size=12, font_color='black', font_weight='bold')
+
+    _ = ax.axis('off')
