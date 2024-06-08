f1cf09ba6ba12e40e0d0174932f08e793290db4b
LM1997610 <115407939+LM1997610@users.noreply.github.com>
2024-06-08 16:57:53 +0200

    Update visualize.py

--- Homework/visualize.py
+++ Homework/visualize.py
@@ -22,3 +22,26 @@ def show_mincut(my_graph, edges_to_color):
     text = nx.draw_networkx_labels(my_graph, pos, font_size=12, font_color='black', font_weight='bold')
 
     _ = ax.axis('off')
+
+
+def show_sparsest_cut(my_graph, nodi_1, nodi_2):
+
+    fig, ax = plt.subplots(figsize=(5,4))
+
+    pos = pos = nx.spring_layout(my_graph, pos=fixed_position, fixed = fixed_position.keys())
+
+    nx.draw_networkx_nodes(my_graph, nodelist = nodi_1, node_size=750, alpha = 0.15, node_color='green', pos = pos)
+    nx.draw_networkx_nodes(my_graph, nodelist = nodi_2, node_size=750, alpha = 0.15, node_color='blue', pos = pos)
+
+    common = [(u,v) for u,v in my_graph.edges(nodi_1) if u in nodi_1 and v in nodi_2]
+    nx.draw_networkx_edges(my_graph, edgelist=common, edge_color = "red", pos=pos, style=':', )
+
+    nx.draw_networkx_edges(my_graph, edgelist=[(u,v) for u,v in my_graph.edges(nodi_1) if u and v in nodi_1],
+                            edge_color = "#8fce00", pos=pos)
+
+    nx.draw_networkx_edges(my_graph, edgelist=[(u,v) for u,v in my_graph.edges(nodi_2) if u and v in nodi_2],
+                            edge_color = "#8e7cc3", pos=pos)
+
+    text = nx.draw_networkx_labels(my_graph, pos, font_size=12, font_color='black', font_weight='bold')
+
+    _ = ax.axis('off')
