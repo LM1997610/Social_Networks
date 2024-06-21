1db7b9ccc54061b26f20791cc7838b04a97d1fec
LM1997610 <115407939+LM1997610@users.noreply.github.com>
2024-06-21 15:50:49 +0200

    Update visualize.py

--- Project/visualize.py
+++ Project/visualize.py
@@ -1,8 +1,13 @@
+
+import os
+from tabulate import tabulate
+
 import matplotlib.pyplot as plt
 
 
 
-def visualize_degree_distrib(nodes, degree_distrib):
+
+def visualize_degree_distrib(nodes, degree_distrib, output_dir='plot_folder/'):
   fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(12, 3))
   
   ax1.plot(nodes, degree_distrib, '-', linewidth=2.5)
@@ -19,10 +24,16 @@ def visualize_degree_distrib(nodes, degree_distrib):
   ax2.grid()
   
   plt.subplots_adjust(wspace=0.3)
+  plt.tight_layout()
+
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  plt.savefig(output_dir+"degree_distrib.png")
   plt.show()
 
 
-def show_length_of_paths(sorted_counter):
+def show_length_of_paths(sorted_counter, output_dir='plot_folder/'):
 
   new_dict = {str(key): value for key, value in sorted_counter.items()}
   new_dict["path\nnot\nfound"] = new_dict.pop('0')
@@ -37,4 +48,47 @@ def show_length_of_paths(sorted_counter):
   plt.xlabel("Length of Paths \n", fontsize= 14)
   plt.ylabel("N. of Articles \n", fontsize= 14)
   plt.bar(keys[-1], values[-1], color='orange')
+  plt.tight_layout()
+
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  plt.savefig(output_dir+"length_of_paths.png")
   plt.show()
+
+
+
+def do_a_plot(data, total_nodes, output_dir='plot_folder/'):
+
+  table_data = []
+
+  for key, value_list in data.items():
+    
+    percentages = [str((round((size / total_nodes) * 100, 2)))+" %" for size in value_list]
+    table_data.append([key] + percentages)
+
+    plt.plot(range(len(value_list)), value_list, alpha = 0.65, label=key)
+
+  headers = ["Node"] + [f"Hop {i+1}" for i in range(len(result))]
+  this_table = tabulate(table_data, headers=headers, tablefmt="pretty")
+
+  plt.gca().spines['right'].set_color('none')
+  plt.gca().spines['top'].set_color('none')
+
+  plt.title("\n\n")
+  plt.xlabel("\n Degrees of Separation \n", fontsize=14)
+  plt.ylabel("N. of Articles \n", fontsize= 14)
+
+
+  plt.grid()
+  plt.legend(loc ="lower right", prop={'size': 9})
+  plt.tight_layout()
+
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  plt.savefig(output_dir+"degrees_separation.png")
+  plt.show()
+
+  return data, this_table
+
