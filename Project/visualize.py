08daa9622e0f375747dee9a3ad35c0af207b6c98
LM1997610 <115407939+LM1997610@users.noreply.github.com>
2024-06-24 01:16:09 +0200

    Update visualize.py

--- Project/visualize.py
+++ Project/visualize.py
@@ -154,57 +154,40 @@ def dead_end_plot(G, dead_node, names, output_directory='plot_folder'):
   #plt.show()
 
 
-def show_subnet(H, node_labels, topic_key, pos, show=False, output_directory='plot_folder/community', ):
+def show_subnet(my_class, show, output_directory='plot_folder/community'):
 
-    isolated_nodes = [node for node in H.nodes if H.degree(node) == 0]
-    
-    if isolated_nodes: 
-      H.remove_nodes_from(isolated_nodes)
+    this_graph = my_class.subgraph.to_undirected()
 
-      node_labels = {k: v for k, v in node_labels.items() if k not in isolated_nodes}
+    palette = plt.get_cmap('tab20', my_class.n_of_subclass)
+    colors = [palette(i) for i in range(my_class.n_of_subclass)]
 
-    #print(f'{"["+topic_key.split()[0]:>12}] Nodes: {str(len(list(H.nodes()))):>4} | Edges: {str(len(list(H.edges()))):>5}', end= " | ")
-  
-    palette = plt.get_cmap('tab20', len(set(node_labels.values())))
-    
-    colors = [palette(i) for i in range(len(set(node_labels.values())))]
+    unique_labels = list(set(my_class.node_labels.values()))
+    color_match = dict(zip(unique_labels, colors))
 
-    color_match = {node: colors[i] for i, node in enumerate(set(node_labels.values()))}
-    node_colors = {k:color_match[value] for k, value in node_labels.items() }
-
-    #print(f'Subclasses: {len(set(node_labels.values()))}')
-  
-    #pos = nx.spring_layout(H)
+    node_colors = {k: color_match[v] for k, v in my_class.node_labels.items()}
 
     plt.figure(figsize=(10, 5))
 
-    #nx.draw_networkx_nodes(H, node_size=10, alpha = 0.65, node_color=list(node_colors.values()), pos = pos)
-    #nx.draw_networkx_edges(H, edge_color = "gray", alpha=0.1 ,  pos=pos)
-
-    nx.draw_networkx_nodes(H, pos, node_color = list(node_colors.values()), node_size=25, alpha=0.70)
-    nx.draw_networkx_edges(H, pos, edge_color = "gray", alpha=0.5)
-                     
-    legend_labels = {cat+ ": "+ str(Counter(node_labels.values())[cat]): color for i, (cat, color) in enumerate(color_match.items())}
+    nx.draw_networkx_nodes(this_graph, my_class.pos, node_color = list(node_colors.values()), node_size=25, alpha=0.70)
+    nx.draw_networkx_edges(this_graph, my_class.pos, edge_color = "gray", alpha=0.25, style="-")
+    
+    legend_labels = {topic + ": "+ str(my_class.topic_count[topic]): color for i, (topic, color) in enumerate(color_match.items())}
 
     plt.legend(handles=[plt.Line2D([], [], color=color, marker='o', linestyle='', markersize=5, label=cat) for cat, color in legend_labels.items()], 
               loc='upper left', bbox_to_anchor=(0.95,0.80), prop={'size': 6.2})
 
 
-    plt.title(f"'{topic_key.split()[0]}' Articles from Wikipedia")
+    plt.title(f"'{my_class.topic_name}' Articles from Wikipedia")
     plt.axis('off')
     plt.tight_layout()
 
     if not os.path.exists(output_directory):
       os.makedirs(output_directory)
 
-    plt.savefig(output_directory + f'/{topic_key.split()[0].lower()}_network.png')
+    plt.savefig(output_directory + f'/{my_class.topic_name.lower()}_network.png')
 
     if show: plt.show()
-    else: 
-      plt.close()
-      print(f'{"["+topic_key.split()[0]:>12}] Nodes: {str(len(list(H.nodes()))):>4} | Edges: {str(len(list(H.edges()))):>5}', end= " | ")
-      print(f'Subclasses: {len(set(node_labels.values()))}')
-
+    else: plt.close()
 
 def in_out_plot(deg, in_deg, out_deg, g_name, output_directory='plot_folder'):
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
